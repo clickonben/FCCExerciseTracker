93e8c9db2ac5a2bae6776ee59d6434d6d042675d
Ben <ben@click-on.co.uk>
2023-03-16 22:11:46 +0000

    Bug fix and renamed file

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 import express  from 'express';
 import cors from 'cors';
-import './endpoints.js';
-import path from 'path';
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+import routes from './routes.js';
 
 const app = express();
 
 app.use(cors())
 app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+app.use('/', routes);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 import db from './database.js';
 import express  from 'express';
 import bodyparser from 'body-parser';
+import path from 'path';
 
 const app = express();
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 app.use(bodyparser.urlencoded({extended: false}));
 
@@ -12,12 +14,12 @@ app.get('/', (req, res) => {
 
 app.post('/api/users/:_id/exercises', async (req, res) => {
     const date = req.body.date ?? new Date();
-    const exercise = await db.CreateExercise(req.params._id, req.body.description, rqe.body.duration, date);
+    const exercise = await db.createExercise(req.params._id, req.body.description, rqe.body.duration, date);
     res.json(exercise); 
 })  
 
 app.post('/api/users', async (req, res) => {
-    const user = await db.createUser(rec.body.username);
+    const user = await db.createUser(req.body.username);
     res.json(user);  
 })
 
@@ -25,3 +27,6 @@ app.get('/api/users/:_id/logs', async (req, res) => {
     const user = await db.getExcercisesForUser(req.body.username, req.body.from, req.body.to, req.body.limit);
     res.json(user);    
 })
+
+export default app;
+
