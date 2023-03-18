3b02ca84cb770132e0896119c5638818048ebd2a
ben <ben@click-on.co.uk>
2023-03-18 03:49:58 +0000

    fixed probem with virtuals not serialising.

--- routes.js
+++ routes.js
@@ -2,9 +2,14 @@ import dbAccess from './data_access.js';
 import express  from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const _dirname = path.dirname(new URL(import.meta.url).pathname);
+//const _dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const filePath = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(filePath);
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -14,8 +19,8 @@ app.get('/', (req, res) => {
 
 app.post('/api/users/:_id/exercises', async (req, res) => {
     const date = req.body.date ?? new Date();
-    const exercise = await dbAccess.createExercise(req.params._id, req.body.description, req.body.duration, date);
-    res.json(exercise); 
+    const user = await dbAccess.createExercise(req.params._id, req.body.description, req.body.duration, date);
+    res.json(user.toJSON({ virtuals: true })); 
 })  
 
 app.route('/api/users/').post( async (req, res) => {
@@ -23,12 +28,12 @@ app.route('/api/users/').post( async (req, res) => {
     res.json(user);  
 }).get(async (req, res) => {
     const users = await dbAccess.getUsers();
-    res.json(users);
+    res.json(users.map(user => user.toJSON({ virtuals: true })));
 })
 
 app.get('/api/users/:_id/logs', async (req, res) => {
     const user = await dbAccess.getExercisesForUser(req.params._id, req.query.from, req.query.to, req.query.limit);
-    res.json(user);    
+    res.json(user.toJSON({ virtuals: true }));    
 })
 
 export default app;
